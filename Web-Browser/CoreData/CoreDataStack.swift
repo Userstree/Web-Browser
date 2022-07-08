303a2f5e517ff30fd59160e113a7d486696453bd
Dossymkhan <doszlmnv@gmail.com>
2022-07-08 10:26:51 +0600

    TODO: Table, coredata 50%

---
+++ Web-Browser/CoreData/CoreDataStack.swift
@@ -0,0 +1,35 @@
+//
+// Created by Dossymkhan Zhulamanov on 08.07.2022.
+//
+
+import Foundation
+import CoreData
+
+class CoreDataStack {
+    private let modelName: String
+
+    init(modelName: String) {
+        self.modelName = modelName
+    }
+
+    private lazy var storeContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: self.modelName)
+        container.loadPersistentStores { _, error in
+            if let error = error as NSError? {
+                print("Unresolved error \(error), \(error.userInfo)")
+            }
+        }
+        return container
+    }()
+
+    lazy var managedContext: NSManagedObjectContext = storeContainer.viewContext
+
+    func saveContext() {
+        guard managedContext.hasChanges else { return }
+        do {
+            try managedContext.save()
+        } catch let error as NSError {
+            print("Unresolved error \(error), \(error.userInfo)")
+        }
+    }
+}
